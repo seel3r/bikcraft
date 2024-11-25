f717c51dd80cf66eef46c42b42afaa80defb794a
Sérgio Seeler <sergioseeler@gmail.com>
2024-11-25 19:12:49 -0300

    menu-ativo-e-parametros-url

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do Orçamento
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(paramentro) {
+  const elemento = document.getElementById(paramentro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+paramentros.forEach(ativarProduto);
