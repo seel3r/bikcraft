a367ceec45a4ffb36cb55f5c02d1d365dff95225
Sérgio Seeler <sergioseeler@gmail.com>
2024-11-25 22:03:03 -0300

    galeria-animacao-e-javascript-desativado

--- js/script.js
+++ js/script.js
@@ -42,3 +42,27 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeira de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(e) {
+  const img = e.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
+
